9876dcf39822c8b750726f5952ffd21292b95bda
AndyLocks <jandylokc@gmail.com>
2025-12-10 03:45:36 +0100

    Delete: unnecessary import

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -3,13 +3,12 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, IsTerminal, Read, Write};
 use std::path::PathBuf;
 use std::process::exit;
-use std::{fs, io};
+use std::io;
 
 pub fn get(dir: Option<PathBuf>, key: &str, enable_safe_format: bool) -> std::io::Result<()> {
     let dir = unwrap_dir(dir);
 
     if !dir.exists() {
-
         eprintln!("The directory {dir:?} does not exist.");
         exit(1)
     }
